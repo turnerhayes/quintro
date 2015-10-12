953569df300ac47778a3c1348cd77b9526b53747
Turner Hayes <dev@turnerhayes.com>
2015-10-12 17:39:09 -0400

    Added access logging to websocket server

--- apps/socket/app.js
+++ apps/socket/app.js
@@ -1,12 +1,28 @@
 "use strict";
 
-var express       = require('express');
 var http          = require('http');
+var fs            = require('fs');
+var path          = require('path');
+var express       = require('express');
+var logger        = require('morgan');
 var SocketManager = require('../../lib/socket-manager');
+var appConfig     = require('../../config/app');
+var pathsConfig   = require('../../config/paths');
 
 var app = express();
 
 var server = http.createServer(app);
+
+app.use(
+	logger(
+		appConfig.logging.format || 'combined',
+		{
+			stream: fs.createWriteStream(
+				path.join(pathsConfig.logs, 'websocket-access.log'), {flags: 'a'}
+			)
+		}
+	)
+);
 
 SocketManager.initialize({
 	server: server,
