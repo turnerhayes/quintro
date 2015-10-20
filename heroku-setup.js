cf4177265f7b9f42b901161ead05320de7a58262
Turner Hayes <dev@turnerhayes.com>
2015-10-19 21:14:04 -0400

    Added defaulting code to Heroku setup to allow heroku local

--- heroku-setup.js
+++ heroku-setup.js
@@ -4,9 +4,10 @@ process.env.DATA_DB_URI               = process.env.DATA_DB_URI || process.env.M
 process.env.SESSION_STORE_URI         = process.env.DATA_DB_URI;
 process.env.ENVIRONMENT               = process.env.ENVIRONMENT || process.env.NODE_ENV;
 process.env.NODE_ENV                  = process.env.ENVIRONMENT;
-process.env.APP_ADDRESS_IS_SECURE     = true;
-process.env.APP_ADDRESS_HOST          = "quintro.herokuapp.com";
-process.env.APP_ADDRESS_EXTERNAL_PORT = '';
+process.env.APP_ADDRESS_IS_SECURE     = process.env.APP_ADDRESS_IS_SECURE !== undefined ?
+	process.env.APP_ADDRESS_IS_SECURE :
+	true;
+process.env.APP_ADDRESS_HOST          = process.env.APP_ADDRESS_HOST || "quintro.herokuapp.com";
 process.env.USE_ENVIRONMENT_CONFIG    = true;
 process.env.LOGGING_USE_CONSOLE       = true;
 process.env.WEB_SOCKETS_INLINE        = true;
