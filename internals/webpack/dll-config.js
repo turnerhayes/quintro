7183a11d251ac8d107ae59b7426184916a197e10
Turner Hayes <dev@turnerhayes.com>
2018-03-09 20:23:08 -0500

    Fixed broken selector unit tests

---
+++ internals/webpack/dll-config.js
@@ -0,0 +1,33 @@
+exports = module.exports = {
+	path: "node_modules/react-boilerplate-dlls",
+	exclude: [
+		"chalk",
+		"compression",
+		"cross-env",
+		"express",
+		"ip",
+		"minimist",
+		"sanitize.css",
+		"rfr",
+		"mongoose",
+		"mongodb",
+		"mkdirp",
+		"nodemon",
+		"dotenv",
+		"morgan",
+		"winston",
+		"cors",
+		"passport",
+		"passport-facebook",
+		"passport-twitter",
+		"passport-google-oauth20",
+		"express-session",
+		"connect-mongo",
+		"cookie-parser"
+	],
+	include: [
+		"core-js",
+		"lodash",
+		"eventsource-polyfill"
+	]
+};
