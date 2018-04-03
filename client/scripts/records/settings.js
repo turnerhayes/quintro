c8754a28029e10f9ab0f0af3b3e2adee6790600a
Turner Hayes <dev@turnerhayes.com>
2018-04-03 19:55:20 -0400

    Removed client directory since it was replaced by app directory

--- client/scripts/records/settings.js
+++
@@ -1,9 +0,0 @@
-import { Record } from "immutable";
-
-const schema = {
-	enableSoundEffects: false,
-	enableNotifications: false,
-	wasRehydrated: false
-};
-
-export default class SettingsRecord extends Record(schema, "Settings") {}
