ea7af94a92721b83e37b0c1748a4c3ad2f1ddf36
Turner Hayes <dev@turnerhayes.com>
2018-04-02 20:26:15 -0400

    Fix service worker configuration

--- server/middlewares/addProdMiddlewares.js
+++ server/middlewares/addProdMiddlewares.js
@@ -12,13 +12,17 @@ const { getIndex, handleGetIndexError } = require("./utils");
 
 module.exports = function addProdMiddlewares(app, options) {
 	const publicPath = options.publicPath || "/";
-	const outputPath = options.outputPath || path.resolve(Config.paths.root, "build");
+	const outputPath = options.outputPath || path.resolve(Config.paths.root, "dist");
 
 	// compression middleware compresses your server responses which makes them
 	// smaller (applies also to assets). You can read more about that technique
 	// and other good practices on official Express.js docs http://mxs.is/googmy
 	app.use(compression());
 	app.use(publicPath, express.static(outputPath));
+
+	app.get("/service-worker/sw.js", (req, res) => {
+		res.sendFile(path.join(outputPath, "service-worker", "sw.js"));
+	});
 
 	app.get("*", (req, res, next) => {
 		getIndex({
