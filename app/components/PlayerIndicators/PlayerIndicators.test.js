b503d19cc8c2556ad60adb5ebd553e094f6b9a8c
Turner Hayes <dev@turnerhayes.com>
2018-05-04 19:07:49 -0400

    PlayerIndicators tests

--- app/components/PlayerIndicators/PlayerIndicators.test.js
+++ app/components/PlayerIndicators/PlayerIndicators.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { fromJS } from "immutable";
 import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { MemoryRouter } from "react-router";
+
 import { wrapWithIntlProvider } from "@app/utils/test-utils";
 import _PlayerIndicators from "./index";
 import Marble from "@app/components/Marble";
+
+const mockStore = configureStore();
 
 const PlayerIndicators = wrapWithIntlProvider(_PlayerIndicators);
 
@@ -26,19 +32,31 @@ describe("PlayerIndicators component", () => {
 					userID: "3",
 				},
 			],
+			currentPlayerColor: "blue",
 			playerLimit: 3,
+			playerPresence: {
+				red: true,
+			},
 		});
 
 		const playerUsers = fromJS({
-			1: {},
-			2: {},
-			3: {},
+			1: {
+				id: "1",
+				isMe: true,
+			},
+			2: {
+				id: "2",
+			},
+			3: {
+				id: "3",
+			},
 		});
 
 		const wrapper = mount(
 			<PlayerIndicators
 				game={game}
 				playerUsers={playerUsers}
+				markActive
 			/>
 		);
 
@@ -61,7 +79,9 @@ describe("PlayerIndicators component", () => {
 		});
 
 		const playerUsers = fromJS({
-			1: {},
+			1: {
+				id: "1",
+			},
 		});
 
 		const wrapper = mount(
@@ -76,5 +96,189 @@ describe("PlayerIndicators component", () => {
 		expect(wrapper.find(Marble).at(1)).toHaveProp("color", null);
 		// eslint-disable-next-line no-magic-numbers
 		expect(wrapper.find(Marble).at(2)).toHaveProp("color", null);
+	});
+
+	it("should show a user information popover on clicking an indicator", () => {
+		const gameName = "testgame";
+
+		const game = fromJS({
+			name: gameName,
+			players: [
+				{
+					color: "blue",
+					userID: "1",
+				},
+			],
+			playerLimit: 3,
+		});
+
+		const playerUsers = fromJS({
+			1: {
+				id: "1",
+				name: {
+					display: "Test",
+				},
+			},
+		});
+
+		const state = fromJS({
+			games: {
+				items: {
+					[gameName]: game,
+				},
+			},
+			users: {
+				items: playerUsers,
+			},
+		});
+
+		const store = mockStore(state);
+
+		const wrapper = mount(
+			<Provider
+				store={store}
+			>
+				<MemoryRouter>
+					<PlayerIndicators
+						game={game}
+						playerUsers={playerUsers}
+					/>
+				</MemoryRouter>
+			</Provider>
+		);
+
+		const classes = wrapper.find("PlayerIndicators").prop("classes");
+
+		wrapper.find(`.${classes.item}`).first().simulate("click");
+
+		expect(wrapper.find("Popover")).toHaveProp("open", true);
+	});
+
+	it("should invoke the onIndicatorClicked callback when a player indicator is clicked", () => {
+		const gameName = "testgame";
+		const userID = "1";
+
+		const player = fromJS({
+			color: "blue",
+			userID,
+		});
+
+		const game = fromJS({
+			name: gameName,
+			players: [
+				player,
+			],
+			playerLimit: 3,
+		});
+
+		const playerUsers = fromJS({
+			[userID]: {
+				id: userID,
+				name: {
+					display: "Test",
+				},
+			},
+		});
+
+		const state = fromJS({
+			games: {
+				items: {
+					[gameName]: game,
+				},
+			},
+			users: {
+				items: playerUsers,
+			},
+		});
+
+		const store = mockStore(state);
+
+		const onIndicatorClicked = jest.fn();
+
+		const wrapper = mount(
+			<Provider
+				store={store}
+			>
+				<MemoryRouter>
+					<PlayerIndicators
+						game={game}
+						playerUsers={playerUsers}
+						onIndicatorClicked={onIndicatorClicked}
+					/>
+				</MemoryRouter>
+			</Provider>
+		);
+
+		const classes = wrapper.find("PlayerIndicators").prop("classes");
+
+		wrapper.find(`.${classes.item}`).first().simulate("click");
+
+		expect(onIndicatorClicked).toHaveBeenCalledWith({ selectedPlayer: player });
+	});
+
+	it("should update state correctly when popover is opened and closed", () => {
+		const gameName = "testgame";
+		const userID = "1";
+		const color = "blue";
+
+		const player = fromJS({
+			color,
+			userID,
+		});
+
+		const game = fromJS({
+			name: gameName,
+			players: [
+				player,
+			],
+			playerLimit: 3,
+		});
+
+		const playerUsers = fromJS({
+			[userID]: {
+				id: userID,
+				name: {
+					display: "Test",
+				},
+			},
+		});
+
+		const state = fromJS({
+			games: {
+				items: {
+					[gameName]: game,
+				},
+			},
+			users: {
+				items: playerUsers,
+			},
+		});
+
+		const store = mockStore(state);
+
+		const wrapper = mount(
+			<Provider
+				store={store}
+			>
+				<MemoryRouter>
+					<PlayerIndicators
+						game={game}
+						playerUsers={playerUsers}
+					/>
+				</MemoryRouter>
+			</Provider>
+		);
+
+		const classes = wrapper.find("PlayerIndicators").prop("classes");
+
+		wrapper.find(`.${classes.item}`).first().simulate("click");
+
+		expect(wrapper.find("PlayerIndicators").instance().state)
+			.toHaveProperty("selectedPlayerColor", color);
+
+		wrapper.find("Popover").prop("onClose")();
+
+		expect(wrapper.find("PlayerIndicators").instance().state)
+			.toHaveProperty("selectedPlayerColor", null);
 	});
 });
