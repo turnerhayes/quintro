10c8f2c04cef78df98cf11829906966038ebb2ff
Turner Hayes <dev@turnerhayes.com>
2018-05-15 19:44:08 -0400

    PlayerInfoPopup container unit tests

---
+++ app/containers/PlayerInfoPopup/PlayerInfoPopup.test.js
@@ -0,0 +1,139 @@
+import React from "react";
+import { fromJS } from "immutable";
+import { shallow } from "enzyme";
+import * as immutableMatchers from "jest-immutable-matchers";
+
+import createReducer from "@app/reducers";
+import selectors from "@app/selectors";
+import {
+	changeUserProfile,
+} from "@app/actions";
+import {
+	fetchedGame,
+} from "@app/actions";
+import { mockStore } from "@app/utils/test-utils";
+
+import PlayerInfoPopup from "./PlayerInfoPopup";
+
+beforeAll(() => {
+	jest.addMatchers(immutableMatchers);
+});
+
+describe("PlayerInfoPopup container", () => {
+	it("should pass the correct props", () => {
+		const reducer = createReducer();
+
+		const player = fromJS({
+			userID: "1",
+			color: "blue",
+		});
+
+		const user = fromJS({
+			id: "1",
+			name: "Test Testerson",
+		});
+
+		const game = fromJS({
+			name: "test",
+			board: {
+				width: 10,
+				height: 10,
+				filled: [],
+			},
+			players: [
+				{
+					user,
+					color: player.get("color"),
+				}
+			],
+		});
+
+		const state = [
+			fetchedGame({ game }),
+		].reduce(reducer, undefined);
+
+		const store = mockStore(state);
+
+		const wrapper = shallow(
+			(
+				<PlayerInfoPopup
+					player={player}
+					game={selectors.games.getGame(state, { gameName: game.get("name") })}
+				/>
+			),
+			{
+				context: {
+					store,
+				},
+			}
+		);
+
+		expect(wrapper.prop("playerUser")).toEqualImmutable(user);
+		expect(wrapper.prop("onDisplayNameChange")).toEqual(expect.any(Function));
+	});
+
+	it("should dispatch a changeUserProfile action on display name change", () => {
+		const reducer = createReducer();
+
+		const player = fromJS({
+			userID: "1",
+			color: "blue",
+		});
+
+		const user = fromJS({
+			id: "1",
+			name: "Test Testerson",
+		});
+
+		const game = fromJS({
+			name: "test",
+			board: {
+				width: 10,
+				height: 10,
+				filled: [],
+			},
+			players: [
+				{
+					user,
+					color: player.get("color"),
+				}
+			],
+		});
+
+		const state = [
+			fetchedGame({ game }),
+		].reduce(reducer, undefined);
+
+		const store = mockStore(state);
+
+		jest.spyOn(store, "dispatch");
+
+		const wrapper = shallow(
+			(
+				<PlayerInfoPopup
+					player={player}
+					game={selectors.games.getGame(state, { gameName: game.get("name") })}
+				/>
+			),
+			{
+				context: {
+					store,
+				},
+			},
+		);
+
+		const displayName = "Test Guy";
+
+		wrapper.prop("onDisplayNameChange")({
+			player,
+			displayName,
+		});
+
+		expect(store.dispatch).toHaveBeenCalledWith(changeUserProfile({
+			userID: player.get("userID"),
+			updates: {
+				displayName,
+			},
+		}));
+	});
+});
