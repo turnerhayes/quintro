6d804faa5cff7fc9104d04145b74a9fdcc175c9d
Turner Hayes <dev@turnerhayes.com>
2018-05-22 20:57:21 -0400

    settings reducer unit tests

--- app/reducers/settings.js
+++ app/reducers/settings.js
@@ -3,16 +3,6 @@ import { CHANGE_SETTING } from "@app/actions";
 
 export default function settingsReducer(state = Map(), action) {
 	switch(action.type) {
-		// case REHYDRATE: {
-		// 	const incoming = action.payload.settings;
-
-		// 	if (incoming) {
-		// 		state = state.mergeDeep(incoming);
-		// 	}
-
-		// 	return state.set("wasRehydrated", true);
-		// }
-
 		case CHANGE_SETTING: {
 			return state.merge(action.payload);
 		}
