660804d5933b387096d1f5993662671a90bd3040
Turner Hayes <dev@turnerhayes.com>
2018-05-31 00:54:41 -0400

    Removed unused isLarge prop

--- app/components/LoadingSpinner/LoadingSpinner.js
+++ app/components/LoadingSpinner/LoadingSpinner.js
@@ -1,33 +1,15 @@
 import React             from "react";
-import PropTypes         from "prop-types";
 import Icon              from "material-ui/Icon";
-import createClassHelper from "@app/components/class-helper";
 import                        "@app/fonts/icomoon/style.less";
 import                        "./LoadingSpinner.less";
 
-const classes = createClassHelper("loading-spinner");
 
-export default function LoadingSpinner({
-	isLarge
-}) {
+export default function LoadingSpinner() {
 	return (
 		<Icon
-			{...classes({
-				extra: "icon",
-				modifiers: [
-					isLarge && "large",
-				],
-			})}
+			className="qc-loading-spinner icon"
 		>
 			loading
 		</Icon>
 	);
 }
-
-LoadingSpinner.propTypes = {
-	isLarge: PropTypes.bool,
-};
-
-LoadingSpinner.defaultProps = {
-	isLarge: false,
-};
