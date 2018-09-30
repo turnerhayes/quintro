99f77e45d1d2e7b4cb25c2aca81978f25c74a908
Turner Hayes <dev@turnerhayes.com>
2018-09-29 21:24:17 -0400

    More Sandbox refactors
    - ability to store/restore games to local storage
    - Improve display (make edit controls, move list toggleable)

--- app/components/Sandbox/MoveList/MoveList.js
+++ app/components/Sandbox/MoveList/MoveList.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import ImmutablePropTypes from "react-immutable-proptypes";
 import { withStyles } from "@material-ui/core/styles";
+import RootRef from "@material-ui/core/RootRef";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -12,15 +13,20 @@ import ClearAllIcon from "@material-ui/icons/ClearAll";
 import ColorSwatch from "@app/components/ColorPicker/ColorSwatch";
 
 const styles = {
-	root: {},
+	root: {
+		display: "flex",
+		flexDirection: "column",
+	},
 
 	listHeader: {
 		display: "flex",
 		justifyContent: "space-between",
+		alignItems: "center",
 	},
 
 	list: {
-		listStyle: "none",
+		overflow: "auto",
+		flex: 1,
 	},
 };
 
@@ -30,6 +36,14 @@ class MoveList extends React.PureComponent {
 		game: ImmutablePropTypes.map.isRequired,
 		onSelectMove: PropTypes.func,
 	}
+
+	componentDidUpdate() {
+		if (this.listRef.current) {
+			this.listRef.current.scrollTop = this.listRef.current.scrollHeight;
+		}
+	}
+
+	listRef = React.createRef()
 
 	handleListItemClick = ({ index }) => {
 		this.props.onSelectMove && this.props.onSelectMove({
@@ -41,33 +55,35 @@ class MoveList extends React.PureComponent {
 		const filledCells = this.props.game && this.props.game.getIn(["board", "filledCells"]);
 
 		return (
-			<List
-				classes={{
-					root: this.props.classes.list,
-				}}
-			>
-				{
-					filledCells && filledCells.map(
-						(cell, index) => (
-							<ListItem
-								key={`${JSON.stringify(cell.get("position"))}`}
-								onClick={() => this.handleListItemClick({ index })}
-								button={index < filledCells.size - 1}
-							>
-								<ListItemIcon>
-									<ColorSwatch
-										color={cell.get("color")}
-									/>
-								</ListItemIcon>
-								<ListItemText
-									primary={cell.get("position").toArray().join(", ")}
+			<RootRef rootRef={this.listRef}>
+				<List
+					classes={{
+						root: this.props.classes.list,
+					}}
+				>
+					{
+						filledCells && filledCells.map(
+							(cell, index) => (
+								<ListItem
+									key={`${JSON.stringify(cell.get("position"))}`}
+									onClick={() => this.handleListItemClick({ index })}
+									button={index < filledCells.size - 1}
 								>
-								</ListItemText>
-							</ListItem>
+									<ListItemIcon>
+										<ColorSwatch
+											color={cell.get("color")}
+										/>
+									</ListItemIcon>
+									<ListItemText
+										primary={cell.get("position").toArray().join(", ")}
+									>
+									</ListItemText>
+								</ListItem>
+							)
 						)
-					)
-				}
-			</List>
+					}
+				</List>
+			</RootRef>
 		);
 	}
 
